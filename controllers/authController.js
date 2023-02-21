efdf794e42520993345fde8a93e84949341a655a
faridullah1 <faridullah996@gmail.com>
2023-02-21 12:30:21 -0800

    supplier login

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,20 +9,37 @@ const { Company } = require('../models/companyModel');
 // Utils
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const { Supplier } = require('../models/supplierModel');
 
 exports.login = catchAsync(async (req, res, next) => {
 	const { error } = validate(req.body);
 	if (error) return next(new AppError(error.message, 400));
 
-	let user = await Company.findOne({ where: { email: req.body.email } });
-	if (!user) return next(new AppError('Invalid email or password.', 400));
+	let user;
+	if (req.body.isSupplier) {
+		user = await Supplier.findOne({ where: { email: req.body.email } });
+		if (!user) return next(new AppError('Invalid email or password.', 400));
+	}
+	else {
+		user = await Company.findOne({ where: { email: req.body.email } });
+		if (!user) return next(new AppError('Invalid email or password.', 400));
+	}
 
 	const isValid = await bcrypt.compare(req.body.password, user.password);
 	if (!isValid) return next(new AppError('Invalid email or password.', 400));
 
-	const token  = jwt.sign({ userId: user.companyId, email: user.email, company: true }, process.env.JWT_PRIVATE_KEY, {
-		expiresIn: process.env.JWT_EXPIRY
-	});
+	const token  = jwt.sign(
+		{ 
+			userId: req.body.isSupplier ? user.supplierId : user.companyId, 
+			email: user.email,
+			name: user.name,
+			company: !req.body.isSupplier 
+		}, 
+		process.env.JWT_PRIVATE_KEY, 
+		{
+			expiresIn: process.env.JWT_EXPIRY
+		}
+	);
 
 	res.status(200).json({
 		status: 'success',
@@ -34,6 +51,7 @@ function validate(req) {
 	const schema = Joi.object({
 		email: Joi.string().email().required(),
 		password: Joi.string().required(),
+		isSupplier: Joi.boolean().required()
 	});
 
 	return schema.validate(req); 
